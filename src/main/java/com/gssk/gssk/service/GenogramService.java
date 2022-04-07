7647d5905494162bf24bd83457b43aa08d87d9d9
nomanlike <57402905+nomanlike@users.noreply.github.com>
2022-04-07 20:26:56 +0700

    Update Service
    Added connecting node
    Don't worry. It will return a whole list as a result

--- src/main/java/com/gssk/gssk/service/GenogramService.java
+++ src/main/java/com/gssk/gssk/service/GenogramService.java
@@ -4,6 +4,7 @@ import com.gssk.gssk.model.Genogram;
 import com.gssk.gssk.model.Person;
 import com.gssk.gssk.model.Relative;
 import com.gssk.gssk.repository.GenogramRepository;
+import com.gssk.gssk.repository.RelativeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,21 +19,36 @@ public class GenogramService {
     public Iterable<Genogram> getAllNodes(){return genogramNodeRepository.findAll();}
     public Genogram FindByID(String id){return genogramNodeRepository.findById(id).get();}
 
+    public Genogram ConnectingDot(Genogram target,List<Relative> rel)
+    {
+        RelativeService relativeService=new RelativeService(rel);
+        Genogram result=target;
+        target.setHusband(relativeService.getRelativeByRelation("husband").getId());
+        target.setWife(relativeService.getRelativeByRelation("wife").getId());
+        target.setM_key(relativeService.getRelativeByRelation("mother").getId());
+        target.setF_key(relativeService.getRelativeByRelation("father").getId());
+        return result;
+    }
 
     public List<Genogram> ConvertFromPerson(Relative person)
     {
         List<Relative> relatives= person.getRelativeList();
-        List<Genogram> result=new ArrayList<>();
+        List<Genogram> GenogramList=new ArrayList<>();
         Genogram node=new Genogram(person);
-        result.add(node);
+        Genogram nodeAdd;
 
+        nodeAdd=ConnectingDot(node,person.getRelativeList());
+        GenogramList.add(nodeAdd);
         for (Relative r:relatives
              ) {
             node=new Genogram(r);
-            result.add(node);
+            nodeAdd=ConnectingDot(node,r.getRelativeList());
+            GenogramList.add(nodeAdd);
         }
 
-        return result;
+
+
+        return GenogramList;
     }
     public Genogram updateAttribute(String argv,Genogram target,String attr[])
     {
