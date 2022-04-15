b50ef68270fdc4c2ac221d27ae2c3394e95c0e2a
nomanlike <57402905+nomanlike@users.noreply.github.com>
2022-04-15 20:22:43 +0700

    Update PersonService.java
    updatePerson
    doesn't have update relative list and health record yet.

--- src/main/java/com/gssk/gssk/service/PersonService.java
+++ src/main/java/com/gssk/gssk/service/PersonService.java
@@ -6,6 +6,14 @@ import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
+import javax.persistence.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
 @Service
 public class PersonService {
     @Autowired
@@ -19,8 +27,40 @@ public class PersonService {
         return personRepository.findById(id).get();
     }
 
+    public Person updatePerson(Person person, String field,String item)
+    {
+        Person newPerson=person;
+        switch (field)
+        {
+            case "firstName":newPerson.setFirstName(item);break;
+            case "lastName":newPerson.setLastName(item);break;
+            case "idCard":newPerson.setIdCard(item);break;
+            case "email":newPerson.setEmail(item);break;
+            case "gender":newPerson.setGender(item);break;
+            case "phonenumber":newPerson.setPhoneNumber(item);break;
+            case "birthday":{
+                SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
+                Date updateBirthday= null;
+                try {
+                    updateBirthday = formatter.parse(item);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                if (updateBirthday !=null)
+                    newPerson.setBirthDay(updateBirthday);
+            };break;
+
+        }
+        return newPerson;
+    }
+
+
     @SneakyThrows
     public Person addNewPerson(Person person) {
         return personRepository.save(person);
     }
+
+
+
+
 }
