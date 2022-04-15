3fe77e0d1ff154f1b109573c26b5c5a753b8632c
nomanlike <57402905+nomanlike@users.noreply.github.com>
2022-04-15 20:30:44 +0700

    Update PersonService.java

--- src/main/java/com/gssk/gssk/service/PersonService.java
+++ src/main/java/com/gssk/gssk/service/PersonService.java
@@ -27,29 +27,29 @@ public class PersonService {
         return personRepository.findById(id).get();
     }
 
-    public Person updatePerson(Person person, String field,String item)
+    public Person updatePerson(Person person, String field,List<String> item)
     {
         Person newPerson=person;
+
         switch (field)
         {
-            case "firstName":newPerson.setFirstName(item);break;
-            case "lastName":newPerson.setLastName(item);break;
-            case "idCard":newPerson.setIdCard(item);break;
-            case "email":newPerson.setEmail(item);break;
-            case "gender":newPerson.setGender(item);break;
-            case "phonenumber":newPerson.setPhoneNumber(item);break;
+            case "firstName":newPerson.setFirstName(item.get(0));break;
+            case "lastName":newPerson.setLastName(item.get(0));break;
+            case "idCard":newPerson.setIdCard(item.get(0));break;
+            case "email":newPerson.setEmail(item.get(0));break;
+            case "gender":newPerson.setGender(item.get(0));break;
+            case "phonenumber":newPerson.setPhoneNumber(item.get(0));break;
             case "birthday":{
                 SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
                 Date updateBirthday= null;
                 try {
-                    updateBirthday = formatter.parse(item);
+                    updateBirthday = formatter.parse(item.get(0));
                 } catch (ParseException e) {
                     e.printStackTrace();
                 }
                 if (updateBirthday !=null)
                     newPerson.setBirthDay(updateBirthday);
             };break;
-
         }
         return newPerson;
     }
