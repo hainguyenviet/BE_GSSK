0e1ddbac4a984d11a4ff4c4bb5daad3a7700cb33
lad1999 <anhduy7216@gmail.com>
2022-04-17 10:03:50 +0700

    clear updatePerson

--- src/main/java/com/gssk/gssk/service/PersonService.java
+++ src/main/java/com/gssk/gssk/service/PersonService.java
@@ -5,6 +5,7 @@ import com.gssk.gssk.repository.PersonRepository;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 import javax.persistence.*;
@@ -26,34 +27,6 @@ public class PersonService {
     public Person getPersonById(String id) {
         return personRepository.findById(id).get();
     }
-
-    public Person updatePerson(Person person, String field,List<String> item)
-    {
-        Person newPerson=person;
-
-        switch (field)
-        {
-            case "firstName":newPerson.setFirstName(item.get(0));break;
-            case "lastName":newPerson.setLastName(item.get(0));break;
-            case "idCard":newPerson.setIdCard(item.get(0));break;
-            case "email":newPerson.setEmail(item.get(0));break;
-            case "gender":newPerson.setGender(item.get(0));break;
-            case "phonenumber":newPerson.setPhoneNumber(item.get(0));break;
-            case "birthday":{
-                SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
-                Date updateBirthday= null;
-                try {
-                    updateBirthday = formatter.parse(item.get(0));
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-                if (updateBirthday !=null)
-                    newPerson.setBirthDay(updateBirthday);
-            };break;
-        }
-        return newPerson;
-    }
-
 
     @SneakyThrows
     public Person addNewPerson(Person person) {
