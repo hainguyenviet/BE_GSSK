59b2b3ee714fd8925136d2a0498b7ce9e743d46d
Đặng Lê Tuấn Anh <56706713+dangletuananh69@users.noreply.github.com>
2022-05-30 12:08:06 +0700

    Update Person.java

--- src/main/java/com/gssk/gssk/model/Person.java
+++ src/main/java/com/gssk/gssk/model/Person.java
@@ -18,7 +18,7 @@ public class Person implements Serializable {
     @GeneratedValue(generator="system-uuid")
     @GenericGenerator(name="system-uuid", strategy = "uuid2")
     @Column(name = "id")
-//     @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+//    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private String id;
     @NotNull
     @Column(name="first_name")
