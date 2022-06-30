297ebcbb260c74941194b151fb0d64ad60eae524
lad1999 <anhduy7216@gmail.com>
2022-06-30 21:01:49 +0700

    update

--- src/main/java/com/gssk/gssk/service/PersonService.java
+++ src/main/java/com/gssk/gssk/service/PersonService.java
@@ -1,5 +1,7 @@
 package com.gssk.gssk.service;
 
+import com.gssk.gssk.model.HealthRecord;
+import com.gssk.gssk.model.Illness;
 import com.gssk.gssk.model.Person;
 import com.gssk.gssk.model.Relative;
 import com.gssk.gssk.repository.PersonRepository;
@@ -41,31 +43,57 @@ public class PersonService {
         person.setEmail(personRequest.getEmail());
         person.setPhoneNumber(personRequest.getPhoneNumber());
         person.setGender(personRequest.getGender());
+
+
+        // update health record
+        HealthRecord newHealthRecord = personRequest.getHealthRecord(), healthRecord = person.getHealthRecord();
+        newHealthRecord.setId(healthRecord.getId());
+        List<Illness> newPersonIllness = newHealthRecord.getIllnessList(), personIllness = healthRecord.getIllnessList();
+        Iterator<Illness> it1 = newPersonIllness.iterator(), it2 = personIllness.iterator();
+        while(it1.hasNext() && it2.hasNext()){
+            it1.next().setId(it2.next().getId());
+        }
+        person.setHealthRecord(newHealthRecord);
+
+        // update relative
         List<Relative> newRelativeList = personRequest.getRelativeList();
         List<Relative> relativeList = person.getRelativeList();
         List<Relative> toRemove = new ArrayList<>();
         List<Relative> duplicate = new ArrayList<>();
-
         if (newRelativeList.isEmpty()){
             relativeList.clear();
         }
         else{
-            for (Relative nr : relativeList) {
-                for (Relative r : newRelativeList) {
-                    if (Objects.equals(nr.getRelation(), r.getRelation())) {
-                        nr.setName(r.getName());
-                        nr.setAge(r.getAge());
-                        nr.setGender(r.getGender());
-                        nr.setDeath_age(r.getDeath_age());
-                        nr.setDeathCause(r.getDeathCause());
-                        nr.setFamilyOrder(r.getFamilyOrder());
-                        nr.setFamilyOrderOther(r.getFamilyOrderOther());
-                        nr.setIsDead(r.getIsDead());
-                        nr.setIllnessRelative(r.getIllnessRelative());
-                        duplicate.add(r);
+            for (Relative r : relativeList) {
+                for (Relative nr : newRelativeList) {
+                    if (Objects.equals(r.getRelation(), nr.getRelation())) {
+                        r.setName(nr.getName());
+                        r.setAge(nr.getAge());
+                        r.setGender(nr.getGender());
+                        r.setDeath_age(nr.getDeath_age());
+                        r.setDeathCause(nr.getDeathCause());
+                        r.setFamilyOrder(nr.getFamilyOrder());
+                        r.setFamilyOrderOther(nr.getFamilyOrderOther());
+                        r.setIsDead(nr.getIsDead());
+                        if (nr.getIllnessRelative() != null){
+                            if (!r.getIllnessRelative().isEmpty()){
+                                List<Illness> oldList = r.getIllnessRelative(), newList = nr.getIllnessRelative();
+                                Iterator<Illness> oldIt = oldList.iterator(), newIt = newList.iterator();
+                                while(oldIt.hasNext() && newIt.hasNext()){
+                                    newIt.next().setId(oldIt.next().getId());
+                                }
+                                r.setIllnessRelative(newList);
+                            }else {
+                                r.setIllnessRelative(nr.getIllnessRelative());
+                            }
+                        }
+                        else {
+                            r.getIllnessRelative().clear();
+                        }
+                        duplicate.add(nr);
                         break;
-                    } else if (newRelativeList.indexOf(r) == (newRelativeList.size() - 1) && (!Objects.equals(nr.getRelation(), r.getRelation()))) {
-                        toRemove.add(nr);
+                    } else if (newRelativeList.indexOf(nr) == (newRelativeList.size() - 1) && (!Objects.equals(r.getRelation(), nr.getRelation()))) {
+                        toRemove.add(r);
                     }
                 }
             }
