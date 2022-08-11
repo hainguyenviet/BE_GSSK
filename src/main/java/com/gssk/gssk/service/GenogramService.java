7c58e9decd717f58b2908034059576263f2ed758
nomanlike <57402905+nomanlike@users.noreply.github.com>
2022-08-11 15:41:30 +0700

    Update GenogramService.java

--- src/main/java/com/gssk/gssk/service/GenogramService.java
+++ src/main/java/com/gssk/gssk/service/GenogramService.java
@@ -229,7 +229,7 @@ public class GenogramService {
                                 count=0;
                                 for(String check: direct2)
                                 {
-                                    if (paternalSide.stream().anyMatch(String->String==check))
+                                    if (paternalSide.contains(check))
                                     {
                                         count++;
                                         Mark.add(direct2Age.get(direct2.indexOf(check)));
@@ -256,7 +256,7 @@ public class GenogramService {
                                             }
                                         }
                                     else
-                                    if (count==1&&direct3Paternal.size()>=2)
+                                    if ((count==1) && (direct3Paternal.size()>=2))
                                     {
                                         result.add("UNGTHUVU_CAO");
                                     }
@@ -266,7 +266,7 @@ public class GenogramService {
                                 count=0;
                                 for(String check: direct2)
                                 {
-                                    if (maternalSide.stream().anyMatch(String->String==check))
+                                    if (maternalSide.contains(check))
                                     {
                                         count++;
                                         Mark.add(direct2Age.get(direct2.indexOf(check)));
@@ -293,7 +293,7 @@ public class GenogramService {
                                     }
 
                                 }else
-                                if (count==1&&direct3Maternal.size()>=2)
+                                if ((count==1) && (direct3Maternal.size()>=2))
                                 {
                                     result.add("UNGTHUVU_CAO");
                                 }
