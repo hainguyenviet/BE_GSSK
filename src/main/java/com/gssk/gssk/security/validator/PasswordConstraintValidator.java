b71cf77fc7004915d87c8d5685a0dfef4232c419
lad1999 <51063965+lad1999@users.noreply.github.com>
2022-09-07 11:28:38 +0700

    Update PasswordConstraintValidator.java

--- src/main/java/com/gssk/gssk/security/validator/PasswordConstraintValidator.java
+++ src/main/java/com/gssk/gssk/security/validator/PasswordConstraintValidator.java
@@ -6,12 +6,7 @@ import java.util.Arrays;
 @Service
 public class PasswordConstraintValidator{
     public boolean checkPassword(String password){
-        PasswordValidator validator = new PasswordValidator(Arrays.asList(new LengthRule(8, 24),
-                new CharacterRule(EnglishCharacterData.UpperCase, 1),
-                new CharacterRule(EnglishCharacterData.LowerCase, 1),
-                new CharacterRule(EnglishCharacterData.Digit, 1),
-                new CharacterRule(EnglishCharacterData.Special, 1),
-                new WhitespaceRule()));
+        PasswordValidator validator = new PasswordValidator(Arrays.asList(new LengthRule(8, 24), new WhitespaceRule()));
 
         RuleResult ruleResult = validator.validate(new PasswordData(password));
         if (ruleResult.isValid()){
